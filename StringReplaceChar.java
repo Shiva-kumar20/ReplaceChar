74240f09990a6e9190ce40eb48fd16e71b1b0492
Shiva-kumar20 <shivakumar.630485@gmail.com>
2025-01-24 00:12:28 +0530

    Add files via upload

---
+++ StringReplaceChar.java
@@ -0,0 +1,9 @@
+
+public class StringReplaceChar {
+    public static void main(String[] args) {
+        String kumar = "a";
+        String c = kumar.replace('a', 'n'); 
+        System.out.println(c); // Outputs 'n'
+    }
+}
+
